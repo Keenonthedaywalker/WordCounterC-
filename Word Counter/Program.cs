18c7b1fde00d701822541938ace70218f2eb949f
Keenonthedaywalker <53871946+Keenonthedaywalker@users.noreply.github.com>
2019-11-01 12:05:46 +0200

    Add files via upload
    Counts words

---
+++ Word Counter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Word_Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] delimeters = new char[] {' ', '\r', '\n' };
+
+            string text = "This is a sentence";
+
+            Console.WriteLine(text.Split(delimeters, StringSplitOptions.RemoveEmptyEntries).Length);
+
+
+        }
+    }
+}
